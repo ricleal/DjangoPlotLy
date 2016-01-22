5dabd6138fa70fe7ee82df9e9d1077ec96c771b8
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-01-22 09:30:14 -0500

    Template ready

---
+++ site1/app1/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from . import views
+
+urlpatterns = [
+    # /app1
+    url(r'^$', views.IndexView.as_view(), name='index'),
+]
---
+++ site1/app1/views.py
@@ -0,0 +1,6 @@
+
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
