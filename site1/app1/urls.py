127173f6908f9b8236c0b92f6420ff6b76d749ef
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-01-22 10:43:54 -0500

    2d done

--- site1/app1/urls.py
+++ site1/app1/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # /app1
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^plot1d/$', views.Plot1DView.as_view(), name='plot1d'),
-
+    url(r'^plot2d/$', views.Plot2DView.as_view(), name='plot2d'),
 ]
--- site1/app1/views.py
+++ site1/app1/views.py
@@ -7,10 +7,19 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
 class Plot1DView(TemplateView):
-    template_name = "plot1d.html"
+    template_name = "plot.html"
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(Plot1DView, self).get_context_data(**kwargs)
         context['plot'] = plots.plot1d()
+        return context
+
+class Plot2DView(TemplateView):
+    template_name = "plot.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(Plot2DView, self).get_context_data(**kwargs)
+        context['plot'] = plots.plot2d()
         return context
